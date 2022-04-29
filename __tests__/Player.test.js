b094ce2069c5a9682719a4e30f6e90dc576a8fdc
NicoleSharrock <n.sharrockhayes@gmail.com>
2022-04-29 15:53:57 -0400

    added enemy methods

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,8 +1,32 @@
 const Player = require('../lib/Player');
 
 
+test("gets player's health value", () => {
+    const player = new Player('goblin', 'sword');
+
+    expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()));
+});
+
+test('checks if player is alive or not', () => {
+    const player = new Player('goblin', 'sword');
+
+    expect(player.isAlive()).toBeTruthy();
+
+    player.health = 0;
+
+    expect(player.isAlive()).toBeFalsy();
+});
+
+test("gets player's attack value", () => {
+    const player = new Player('goblin', 'sword');
+    player.strength = 10;
+
+    expect(player.getAttackValue()).toBeGreaterThanOrEqual(5);
+    expect(player.getAttackValue()).toBeLessThanOrEqual(15);
+});
+
 test("subtracts from player's health", () => {
-    const player = new Player('Dave');
+    const player = new Player('goblin', 'sword');
     const oldHealth = player.health;
 
     player.reduceHealth(5);
